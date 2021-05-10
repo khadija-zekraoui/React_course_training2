730a9f7aa069a9807a00dc034710873d7125a589
Khadija Zekraoui <kazu@gft.com>
2021-05-10 12:59:19 +0200

    what Higher Order Components are, and how we can create one

--- higher-order-components/src/components/About.jsx
+++ higher-order-components/src/components/About.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
-
-const Contact = (props) => {
-    
-    setTimeout(() => {
-    //redirect the user ti another page
-      props.history.push('/about')  
-    }, 2000);
+import Rainbow from '../hoc/Rainbow.js'
+const About = () => {
     return (
         <div className="container">
-            <h4 className="center">Contact</h4>
+            <h4 className="center">About</h4>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
     )
 }
 
-export default Contact
+export default Rainbow(About)
